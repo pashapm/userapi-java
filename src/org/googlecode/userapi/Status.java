ee1e3b36274b603c31c47829a0621e2ad71db511
ildar.karimov <ildar.karimov@3fd5a652-8d5c-11de-927e-ab586cd9c9de>
2009-09-02 21:38:20 +0000

    simpe status query

---
+++ src/org/googlecode/userapi/Status.java
@@ -0,0 +1,60 @@
+package org.googlecode.userapi;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.Date;
+
+public class Status {
+    private long statusId;
+    private long userId;
+    private int reserved;//0, reserved (rather user's gender?)
+    private String userName;
+    private Date date;
+    private String text;
+
+    public Status(JSONArray statusJson) throws JSONException {
+        statusId = Long.parseLong(statusJson.getString(0).split("_")[1]);
+        userId = statusJson.getLong(1);
+//        reserved = statusJson.getInt(2);
+        userName = statusJson.getString(3);
+        date = new Date(statusJson.getLong(4)*1000);
+        text = statusJson.getString(5);
+    }
+
+    public long getStatusId() {
+        return statusId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public int getReserved() {
+        return reserved;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String toString() {
+        return "Status{" +
+                "statusId=" + statusId +
+                ", userId=" + userId +
+                ", reserved=" + reserved +
+                ", userName='" + userName + '\'' +
+                ", date=" + date +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
