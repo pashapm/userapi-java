81a63a89561524bcb39f2270372e27d00a30734e
ildar.karimov <ildar.karimov@3fd5a652-8d5c-11de-927e-ab586cd9c9de>
2009-09-12 00:48:44 +0000

    automatic login/session handling

---
+++ src/org/googlecode/userapi/Credentials.java
@@ -0,0 +1,48 @@
+package org.googlecode.userapi;
+
+public class Credentials {
+    private String login;
+    private String pass;
+    private String remixpass;
+    private String session;
+
+    public Credentials(String login, String pass, String remixpass, String session) {
+        if (login==null || pass==null) throw new IllegalArgumentException("login/pass must not be null");
+        this.login = login;
+        this.pass = pass;
+        this.remixpass = remixpass;
+        this.session = session;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getRemixpass() {
+        return remixpass;
+    }
+
+    public void setRemixpass(String remixpass) {
+        this.remixpass = remixpass;
+    }
+
+    public String getSession() {
+        return session;
+    }
+
+    public void setSession(String session) {
+        this.session = session;
+    }
+}
